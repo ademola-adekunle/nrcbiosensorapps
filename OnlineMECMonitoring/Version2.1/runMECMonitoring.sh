350596745981ac56421e9b157a09bb6783114e53
midas-chin <midaschin@gmail.com>
2021-05-05 23:37:18 -0400

    Test new version

--- OnlineMECMonitoring/Version2.1/runMECMonitoring.sh
+++ OnlineMECMonitoring/Version2.1/runMECMonitoring.sh
@@ -2,5 +2,5 @@
 sleep 5
 cd /home/pi/.local/share
 cd "OnlineMECMonitoring"
-cd "Version2"
-python3 "/home/pi/.local/share/OnlineMECMonitoring/Version2/KORAD_PS_DAQ.py"
+cd "Version2.1"
+python3 "/home/pi/.local/share/OnlineMECMonitoring/Version2.1/KORAD_PS_DAQ.py"
