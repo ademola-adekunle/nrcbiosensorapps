b89fa56f4060900f5aa5c1d4e23bf593585f31cc
midas-chin <midaschin@gmail.com>
2021-05-12 20:30:10 -0400

    Script change

--- OnlineBiosensorMCU/Version 1/Source files/runBiosensor.sh
+++ OnlineBiosensorMCU/Version 1/Source files/runBiosensor.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 sleep 5
 cd $HOME
-cd ..
-cd ..
-cd usr
-cd share
-cd "Source files"
-sudo -E  python3 "/usr/share/Source files/BiosensorMain.py"
+cd "/home/pi/.local/share/OnlineBiosensorMCU/Version 1/Source files"
+python3 "/home/pi/.local/share/OnlineBiosensorMCU/Version 1/Source files/BiosensorMain.py"
