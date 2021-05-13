7a3137ea4290b04e8628577bec4b3c546ea3b6ce
midas-chin <midaschin@gmail.com>
2021-05-12 23:02:51 -0400

    run script fix

--- OnlineBiosensorMCU/Version 1/Source files/runBiosensor.sh
+++ OnlineBiosensorMCU/Version 1/Source files/runBiosensor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sleep 5
+sleep 7
 cd $HOME
 cd "/home/pi/.local/share/OnlineBiosensorMCU/Version 1/Source files"
 python3 "/home/pi/.local/share/OnlineBiosensorMCU/Version 1/Source files/BiosensorMain.py"
